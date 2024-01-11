3801ebccda3a5a7071ad9efa53863cd639e1b683
nxnrrr <125960882+nxnrrr@users.noreply.github.com>
2024-01-11 17:22:49 +0100

    Update Confirmer_mdp.jsx

--- src/pages/Confirmer_mdp.jsx
+++ src/pages/Confirmer_mdp.jsx
@@ -43,29 +43,29 @@ function Confirmer_mdp() {
   };
   
   return (
-    <div className="w-full  flex items-start ">
+  <div className="w-full min-h-screen flex items-start ">
     <div className="w-0 xl:w-1/2  h-screen flex items-center justify-center">
-      <img src={pic} alt=''  className="h-full w-full object-cover"/>
+      <img src={pic} alt=''  className="h-full  w-full object-cover"/>
      </div>
-    <div  className="flexbox flex-col w-full xl:w-1/2 h-screen flex items-center justify-center  ">
-    <div className=" h-12 w-full items-center bg-customBlue opacity-5 p-2 shadow-2xl rounded-full filter blur-md  "></div>
-     <div className="w-[650px] xl:w-[726px] h-[901px] flex flex-col items-center justify-evenly  bg-white  shadow-2xl  rounded-2xl ">
+    <div  className=" w-full xl:w-1/2 flexbox flex-col   h-screen  flex items-center justify-center  ">
+    <div className="hidden xl:block h-12 w-full items-center bg-customBlue opacity-5 p-2 shadow-2xl rounded-full filter blur-md  "></div>
+    <div className="flex flex-col w-full xl:w-[640px] xl:h-[901px] h-full items-center justify-evenly bg-white  md:shadow-2xl  rounded-2xl ">
      <img
       src={pic2}
       alt="Logo" 
-      className='w-15 h-9 mt-10 mb-8 px-0 py-0  bg-transparent '
+      className='w-15 h-9  px-0 py-0  bg-transparent '
      ></img>
-     < div className='flex flex-col items-center justify-between gap-4 py-4'>
-     <div className="w-[277px] h-[29px] text-center text-slate-700 text-3xl font-extrabold font-['Raleway']">Créer un compte </div>
-     <div className="w-[449px] h-[61px] text-center text-black text-[15px] font-medium font-['Raleway']">Créez un compte en remplissant le formulaire  avec vos informations personnelles<br/></div>
+     < div className='flex flex-col items-center justify-evenly gap-5'>
+     <div className="xl:w-[277px] w-full h-[29px] text-center text-slate-700 text-3xl font-extrabold font-['Raleway']">Créer un compte </div>
+     <div className="xl:w-[449px] w-full h-[61px]  text-center text-black xl:text-[15px] text-[14px] font-medium font-['Raleway']">Créez un compte en remplissant le formulaire  avec vos informations personnelles<br/></div>
      </div>
-     <div className='flex flex-col mt-2 gap-2  justify-evenly items-center'>
+     <div className='flex flex-col flex-wrap sm:w-[530px] w-full gap-2 justify-evenly items-center'>
      <input
      type ="password"
      placeholder='  Mot de passe '
      value={password}
         onChange={handlePasswordChange}
-     className='w-[528px] h-20  my-4 text-[20px] text-black  bg-slate-200  rounded-[10px] '
+     className='sm:w-[528px] w-2/3 h-20 text-black text-[18px]   bg-slate-200 rounded-[10px]  '
      />
     
      <input
@@ -73,16 +73,16 @@ function Confirmer_mdp() {
      placeholder=' Confirmer votre mot de passe  '
      value={confirmPassword}
         onChange={handleConfirmPasswordChange}
-    className='w-[528px] h-20 text-black text-[20px]   bg-slate-200 rounded-[10px] '
+    className='sm:w-[528px] w-2/3 h-20 text-black text-[18px]   bg-slate-200 rounded-[10px] '
      />
      </div>
     
     
 
-     <div className='mb-9'>
+     <div className='flex flex-col flex-wrap sm:w-[530px] w-full gap-2 justify-evenly items-center'>
      <Link to={"/SignIn/Verification/Confirmation"} >
       <button onClick={HandleSignInconf}
-       className="bg-customBlue w-[264px] h-[65px] text-white  font-normal   rounded-tl-[10px] rounded-bl-[10px] rounded-br-[10px]">
+       className="bg-customBlue sm:w-[528px] w-2/3 h-[65px] text-white font-normal rounded-tl-[10px] rounded-bl-[10px] rounded-br-[10px]">
       Confirmer
      </button>
      
@@ -92,10 +92,10 @@ function Confirmer_mdp() {
      {errorConfirmedPassword && <p className="text-red-500">{errorConfirmedPassword}</p>}
      {errorMessage && <p className="text-red-500">{errorMessage}</p>}
        
-     <div className='flex flex-col mb-8  justify-evenly  gap-4'>
-     <div className="w-[397px] h-[0px] opacity-40 border border-slate-700"></div>
-    <div className='flex  w-full  justify-between '>
-    <div className="text-black text-opacity-40 text-[15px] font-['SF Pro']">Vous avez déjà un compte ?</div>
+ < div className='flex flex-col sm:w-[397px] items-center w-full justify-evenly mb-8  '>
+     <div className="sm:w-[397px] w-2/3 h-[0px] opacity-40 border border-slate-700 mb-2"></div>
+     <div className='flex sm:w-[397px] w-2/3 justify-between '>
+     <div className="text-black text-opacity-40 text-[15px] font-['SF Pro']">Vous avez déjà un compte ?</div>
     <Link to={"/LogIn"}>
     <button
                 className="text-slate-700  text-[15px] font-['SF Pro']  text-[15px] "
@@ -106,7 +106,7 @@ function Confirmer_mdp() {
     </div>
      </div>
      </div>
-     <div className="h-12 w-full items-center bg-customRed opacity-10 p-4 shadow-2xl rounded-full filter blur-md  "></div>
+     <div className="hidden xl:block h-12 w-full items-center bg-customRed opacity-10 p-4 shadow-2xl rounded-full filter blur-md  "></div>
     </div>
     
   
